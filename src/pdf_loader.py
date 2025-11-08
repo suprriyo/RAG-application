7b0c5449079dab8363960c8edfbfbcf571a72eec
Supriyo chakraborty <supriyochakraborty954@gmail.com>
2025-11-08 14:10:23 +0530

    Fix langchain import for Streamlit Cloud

--- src/pdf_loader.py
+++ src/pdf_loader.py
@@ -3,7 +3,7 @@
 import os
 from typing import List
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.schema import Document
+from langchain_core.documents import Document
 
 
 class PDFProcessingError(Exception):
